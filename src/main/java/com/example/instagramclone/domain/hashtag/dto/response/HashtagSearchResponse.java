e5607db9158b59595681e468245523a31279523b
mabbakk <siyun145928@gmail.com>
2025-01-07 11:17:29 +0900

    #41 [BE/feat] make hashtag search API

---
+++ src/main/java/com/example/instagramclone/domain/hashtag/dto/response/HashtagSearchResponse.java
@@ -0,0 +1,18 @@
+package com.example.instagramclone.domain.hashtag.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@NoArgsConstructor
+public class HashtagSearchResponse {
+
+
+    private String hashtag;
+    private int feedCount;
+
+
+}
